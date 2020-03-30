4a1daee5e0757ef2eb5c0d75704335a8870548db
Samuel Martinez <semartinez147@gmail.com>
2020-03-30 16:44:47 -0600

    Added textureview for camera display + camera boilerplate.

--- app/src/main/java/edu/cnm/deepdive/scavengrclient/CurrentClueFragment.java
+++ app/src/main/java/edu/cnm/deepdive/scavengrclient/CurrentClueFragment.java
@@ -1,14 +1,49 @@
 package edu.cnm.deepdive.scavengrclient;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.TextureView.SurfaceTextureListener;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import java.util.Arrays;
 
 public class CurrentClueFragment extends Fragment {
 
+  private TextureView cameraFrame;
+  private String cameraId;
+  private CameraManager cameraManager;
+  private Size imageDimension;
+  private static final int REQUEST_CAMERA_PERMISSION = 200;
+  protected CaptureRequest captureRequest;
+  protected CaptureRequest.Builder captureRequestBuilder;
+  protected CameraDevice cameraDevice;
+  protected CameraCaptureSession cameraCaptureSessions;
+  private Handler backgroundHandler;
+  private HandlerThread backgroundThread;
 
   public CurrentClueFragment() {
     // Required empty public constructor
@@ -22,4 +57,164 @@ public class CurrentClueFragment extends Fragment {
     return inflater.inflate(R.layout.fragment_current_clue, container, false);
   }
 
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    cameraFrame = view.findViewById(R.id.camera_frame);
+    cameraFrame.setSurfaceTextureListener(textureListener);
+  }
+
+
+// Camera methods below
+
+  SurfaceTextureListener textureListener = new SurfaceTextureListener() {
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+      // open camera
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+      // transform image
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+      return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+      // handle event
+    }
+  };
+
+  private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+    @Override
+    public void onOpened(CameraDevice camera) {
+      //This is called when the camera is open
+      cameraDevice = camera;
+      createCameraPreview();
+    }
+    @Override
+    public void onDisconnected(CameraDevice camera) {
+      cameraDevice.close();
+    }
+    @Override
+    public void onError(CameraDevice camera, int error) {
+      cameraDevice.close();
+      cameraDevice = null;
+    }
+  };
+
+  protected void startBackgroundThread() {
+    backgroundThread = new HandlerThread("Camera Background");
+    backgroundThread.start();
+    backgroundHandler = new Handler(backgroundThread.getLooper());
+  }
+
+  protected void stopBackgroundThread() {
+    backgroundThread.quitSafely();
+    try {
+      backgroundThread.join();
+      backgroundThread = null;
+      backgroundHandler = null;
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  protected void createCameraPreview() {
+    try {
+      SurfaceTexture texture = cameraFrame.getSurfaceTexture();
+      assert texture != null;
+      texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
+      Surface surface = new Surface(texture);
+
+      //replace with QR capture?
+      captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+      captureRequestBuilder.addTarget(surface);
+
+      cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback(){
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+          //The camera is already closed
+          if (null == cameraDevice) {
+            return;
+          }
+          // When the session is ready, we start displaying the preview.
+          cameraCaptureSessions = cameraCaptureSession;
+          updatePreview();
+        }
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+          Toast.makeText(getActivity(), "Configuration change", Toast.LENGTH_SHORT).show();
+        }
+      }, null);
+    } catch (CameraAccessException e) {
+      e.printStackTrace();
+    }
+  }
+  private void openCamera() {
+    cameraManager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
+    try {
+      cameraId = cameraManager.getCameraIdList()[0];
+      CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+      StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+      assert map != null;
+      imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+      // Add permission check for camera
+      if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CAMERA_PERMISSION);
+        return;
+      }
+      cameraManager.openCamera(cameraId, stateCallback, null);
+    } catch (CameraAccessException e) {
+      e.printStackTrace();
+    }
+  }
+  protected void updatePreview() {
+    captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+    try {
+      cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, backgroundHandler);
+    } catch (CameraAccessException e) {
+      e.printStackTrace();
+    }
+  }
+  private void closeCamera() {
+    if (cameraDevice != null) {
+      cameraDevice.close();
+      cameraDevice = null;
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode == REQUEST_CAMERA_PERMISSION) {
+      if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+        // close the app
+        Toast.makeText(getContext(), "Sorry, you can't use the camera without granting permission", Toast.LENGTH_LONG).show();
+//        finish();
+      }
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Log.e("ClueCam", "onResume");
+    startBackgroundThread();
+    if (cameraFrame.isAvailable()) {
+      openCamera();
+    } else {
+      cameraFrame.setSurfaceTextureListener(textureListener);
+    }
+  }
+  @Override
+  public void onPause() {
+    Log.e("ClueCam", "onPause");
+    closeCamera();
+    stopBackgroundThread();
+    super.onPause();
+  }
 }
