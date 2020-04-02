cf56b6240adc5b3d5872631ede37b5554b50fb46
TraceDecker <decker1041@gmail.com>
2020-04-02 11:25:23 -0600

    implemented click listener for leave hunt button

--- app/src/main/java/edu/cnm/deepdive/scavengrclient/controller/ui/SingleHuntFragment.java
+++ app/src/main/java/edu/cnm/deepdive/scavengrclient/controller/ui/SingleHuntFragment.java
@@ -2,13 +2,16 @@ package edu.cnm.deepdive.scavengrclient.controller.ui;
 
 
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.FragmentManager;
 import edu.cnm.deepdive.scavengrclient.R;
 
-public class SingleHuntFragment extends Fragment {
+public class SingleHuntFragment extends Fragment implements OnClickListener {
 
 
   public SingleHuntFragment() {
@@ -19,8 +22,18 @@ public class SingleHuntFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_single_hunt, container, false);
+    View view = inflater.inflate(R.layout.fragment_single_hunt, container, false);
+    Button button = view.findViewById(R.id.leave_hunt_button);
+    button.setOnClickListener(this);
+
+    return view;
   }
 
+  @Override
+  public void onClick(View v) {
+    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+    fragmentManager.beginTransaction()
+        .add(R.id.nav_join_hunt, new JoinHuntFragment(), null)
+        .commit();
+  }
 }
