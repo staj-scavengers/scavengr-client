b09a001e3272a041843aa0a3ad1f502f4e1e62e6
TraceDecker <decker1041@gmail.com>
2020-04-06 15:39:12 -0600

    added click to finish creation

--- app/src/main/java/edu/cnm/deepdive/scavengrclient/controller/ui/CreateNewHuntFragment.java
+++ app/src/main/java/edu/cnm/deepdive/scavengrclient/controller/ui/CreateNewHuntFragment.java
@@ -2,17 +2,20 @@ package edu.cnm.deepdive.scavengrclient.controller.ui;
 
 
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.navigation.Navigation;
 import edu.cnm.deepdive.scavengrclient.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CreateNewHuntFragment extends Fragment {
+public class CreateNewHuntFragment extends Fragment implements OnClickListener {
 
 
   public CreateNewHuntFragment() {
@@ -23,8 +26,14 @@ public class CreateNewHuntFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_create_new_hunt, container, false);
+    View view = inflater.inflate(R.layout.fragment_create_new_hunt, container, false);
+    Button button = view.findViewById(R.id.complete_create_hunt_button);
+    button.setOnClickListener(this);
+    return view;
   }
 
+  @Override
+  public void onClick(View view) {
+    Navigation.findNavController(view).navigate(R.id.nav_home);
+  }
 }
